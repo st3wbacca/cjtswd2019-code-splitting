0d7f4918cac21ee3f4348d5347d1ac804da63e81
Stuart B <stuart.busilla@gmail.com>
2019-01-03 15:18:13 -0600

    add asynchronous component to load pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Page1 from './components/Page1';
+import asyncComponent from './components/AsyncComponent';
 
 class App extends Component {
   constructor() {
@@ -13,32 +14,18 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
-    if (route === 'page1') {
-      this.setState({route: route});
-    } else if (route === 'page2') {
-      import('./components/Page2')
-        .then((Page2) => {
-          this.setState(
-            { route: route,
-              component: Page2.default,
-            });
-        });
-    } else if (route === 'page3') {
-      import('./components/Page3')
-        .then((Page3) => {
-          this.setState(
-            { route: route,
-              component: Page3.default,
-            });
-        });
-    }
+    this.setState({route: route});
   }
 
   render() {
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />
-    } else {
-      return <this.state.component onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === 'page2') {
+      const AsyncPage2 = asyncComponent(() => import('./components/Page2'));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === 'page3') {
+      const AsyncPage3 = asyncComponent(() => import('./components/Page3'));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />
     }
   }
 }
